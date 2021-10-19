31393c2ab8a8582f5a9f2d48fd4588172aa2cefc
Juan Pablo Rivera Sierra <54080237+Juriveras@users.noreply.github.com>
2021-10-19 07:40:49 -0500

    Subidos primera versión de clases Auto, Motor y Asiento

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,35 @@
+package test;
+
+public class Auto {
+	
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos () {
+		int cant = 0;
+		for (int i = 0; i < asientos.length; i++) {
+			if (asientos[i].getClass() == Asiento.class) {
+				cant++;
+			}
+		}
+		return cant;
+	}
+	
+	String verificarIntegridad () {
+		if (registro != motor.registro) {
+			return "Las piezas no son originales"; 
+		}		
+		for (int i = 0; i < asientos.length; i++) {
+			if (registro != asientos[i].registro) {
+				return "Las piezas no son originales";
+			}
+		}		
+		return "Auto original";
+	}
+
+}
---
+++ src/test/java/test/Motor.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Motor {
+	
+	int numeroCilindros;
+	String tipo;
+	int registro;
+	
+	void cambiarRegistro (int registro) {
+		registro = registro;
+	}
+	
+	void asignarTipo (String tipo) {
+		tipo = tipo;
+	}
+}
